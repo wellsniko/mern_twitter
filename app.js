abd6707cf3cc1d5357e246bd80f985de4b287243
Niko Wells <wellsniko@gmail.com>
2020-12-23 20:34:25 -0800

    Ready for heroku push

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-
+const path = require('path');
 
 const users = require("./routes/api/users");
 const tweets = require("./routes/api/tweets");
@@ -31,3 +31,9 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
