cef64350dfe89d8335e958f7cf6327ee6e3c638f
Niko Wells <wellsniko@gmail.com>
2020-12-25 21:02:33 -0800

    session signup

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -23,8 +23,7 @@ export default function(state = initialState, action) {
     case RECEIVE_USER_SIGN_IN:
       return {
         ...state,
-        isSignedIn: true,
-        isAuthenticated: true
+        isSignedIn: true
       }
     default:
       return state;
