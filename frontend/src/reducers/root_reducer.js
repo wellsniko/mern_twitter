8cba4287469bfb4ff74e521d70cee414c2a68932
Niko Wells <wellsniko@gmail.com>
2020-12-25 20:30:31 -0800

    fix session reducer

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from 'redux';
-import session from './session_reducer';
+import SessionReducer from './session_reducer';
 import errors from './errors_reducer'
 import tweets from './tweets_reducer'
 
 const RootReducer = combineReducers({
-  session, 
+  session: SessionReducer, 
   errors,
   tweets
 });
